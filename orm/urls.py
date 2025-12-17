893e093e3d5842570dca3fa13f2ed0d5083bbd7f
Ravi Kalakoti <72433680+Ravikalakoti@users.noreply.github.com>
2025-12-17 09:26:27 +0530

    tetsing ruff checks

--- orm/urls.py
+++ orm/urls.py
@@ -21,10 +21,10 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-path('admin/', admin.site.urls),
+path('admin/',    admin.site.urls),
 path('', include('library.urls')),
 ]
 
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
